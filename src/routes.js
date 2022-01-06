f9a70919407332172f87fcf6d1823243c774ff73
judith lourde <judith.lourde@gmail.com>
2022-01-07 00:06:06 +0100

    Deleted MenuView.vue

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 import HomeView from './views/HomeView.vue';
-// import MenuView from './views/MenuView.vue';
 
 export default [
 	{ name: 'home', path: '/', component: HomeView },
-	// { name: 'menu', path: '/menu', component: MenuView }	
 ];
