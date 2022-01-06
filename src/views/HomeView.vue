98f9cf527e0cefd7dc3b654f2fe0de4f99e4f5e4
judith lourde <judith.lourde@gmail.com>
2022-01-06 22:15:10 +0100

    Added view files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <Home />
+    </div>
+</template>
+
+<script>
+import Home from '../components/Home.vue';
+
+export default {
+    components: {
+        Home
+    }
+}
+</script>
+
+<style>
+    .home {
+        /* color: aqua; */
+    }
+</style>
---
+++ src/views/MenuView.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- routes to each menu-component -->
+    <!-- <div class="homeAlone"> -->
+        <!-- <Collapsible /> -->
+        <!-- <PizzaMenu /> -->
+        <!-- <BurgerMenu v-if="$route.params.menu_id === 'burgermenu'" />
+        <KebabMenu v-if="$route.params.menu_id === 'kebabmenu'" />
+        <PlateDishesMenu v-if="$route.params.menu_id === 'platadishes-menu'" /> -->
+    <!-- </div> -->
+</template>
+
+<script>
+    // import Collapsible from '../components/Collapsible.vue';
+    // import PizzaMenu from '../components/PizzaMenu.vue';
+    // import BurgerMenu from '../components/BurgerMenu.vue';
+    // import KebabMenu from '../components/KebabMenu.vue';
+    // import PlateDishesMenu from '../components/PlateDishesMenu.vue';
+
+	export default {
+		components: {
+            // Collapsible,
+            // PizzaMenu,
+            // BurgerMenu,
+            // KebabMenu,
+            // PlateDishesMenu,
+		}
+	}
+</script>
+
+<style>
+    .homeAlone {
+        color: tomato;
+    }
+</style>
