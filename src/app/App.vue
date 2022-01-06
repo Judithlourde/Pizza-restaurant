06941e7451d106ef9f5b6ad339d0ad9f38c4e41d
judith lourde <judith.lourde@gmail.com>
2022-01-06 22:14:17 +0100

    Added routerview(App.vue)

---
+++ src/app/App.vue
@@ -0,0 +1,66 @@
+<template>
+	<RouterView />
+   
+	<!-- <div>
+        <Collapsible :title="this.componentTitles.pizzaMenu" class="mobile__component"> 
+            <PizzaMenu /> 
+        </Collapsible>
+
+        <Collapsible :title="this.componentTitles.burgerMenu" class="mobile__component" > 
+            <BurgerMenu /> 
+        </Collapsible>
+
+        <Collapsible :title="this.componentTitles.kebabMenu" class="mobile__component" > 
+            <KebabMenu /> 
+        </Collapsible>
+
+        <Collapsible :title="this.componentTitles.plateDishesMenu" class="mobile__component" > 
+            <PlateDishesMenu /> 
+        </Collapsible>
+    </div>
+    
+    <ScrollingImages />
+    
+    <Footer /> -->
+</template>
+
+
+<script>
+// import Collapsible from '../components/Collapsible.vue';
+// import PizzaMenu from '../components/PizzaMenu.vue';
+// import BurgerMenu from '../components/BurgerMenu.vue';
+// import KebabMenu from '../components/KebabMenu.vue';
+// import PlateDishesMenu from '../components/PlateDishesMenu.vue';
+// import Footer from '../components/Footer.vue';
+// import ScrollingImages from '../components/ScrollingImages.vue';
+
+// export default {
+//     components: {
+//         Collapsible,
+//         PizzaMenu,
+//         BurgerMenu,
+//         KebabMenu,
+//         PlateDishesMenu,
+//         Footer,
+//         ScrollingImages
+//     },
+
+//     data() {
+//         return {
+//             componentTitles: { 
+//                 pizzaMenu: 'Pizza Meny',
+//                 burgerMenu: 'Hambuger Meny',
+//                 kebabMenu: 'Kebab Meny',
+//                 plateDishesMenu: 'Tallerkenretter / Barnemeny / Diverse'   
+//             }
+//         }
+//     }
+// }
+</script>
+
+<style>
+	@import '../style/reset.css';
+	@import '../style/variables.css';
+	@import '../style/fonts.css';
+	@import '../style/style.css';
+</style>
