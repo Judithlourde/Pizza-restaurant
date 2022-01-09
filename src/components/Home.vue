e64ea3ec2b7e9c56aafd503588f181f2b4f29813
judith lourde <judith.lourde@gmail.com>
2022-01-10 00:34:39 +0100

    Included facebook link

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,17 @@
         <nav class="header">
             <h1>Spikkestad Pizza og Grill</h1>
 
-            <div class="header__phone">
-                <img src="/svg/phone.svg" alt="phone-icon">
-                <p>31 28 75 06/ 48 95 96 14</p>
+            <div class="header__contact">
+                <div class="header__contact-phone">
+                    <img src="/svg/phone.svg" alt="phone-icon">
+                    <p>31 28 75 06/ 48 95 96 14</p>
+                </div>
+
+                <div class="header__contact-facebook">
+                    <img src="/svg/facebook.svg" alt="facebook-icon">
+
+                    <a href="https://www.facebook.com/Spikkestad-pizza-og-grill-112698187314811/"><p>følg oss på Facebook for gode tilbud</p></a>
+                </div>    
             </div>
         </nav>
 
@@ -82,21 +90,31 @@
         font-size: 2.5em;
     }
 
-    .header__phone {
+    .header__contact-phone, .header__contact-facebook {
         display: flex;
         flex-direction: row;
         justify-content: center;
         align-items: center;
-        padding-bottom: 18px;
+        padding-bottom: 10px;
     }
 
-    .header__phone > p {
+    .header__contact-phone > p,
+    .header__contact-facebook > a > p {
         padding: 10px;
-        color: white;
+        color: #FBF6E5;
         font-size: 1.25em;
     }
 
-    .header__phone > img {
+    .header__contact-facebook > a {
+        /* text-decoration: none; */
+         color: #FBF6E5;
+    }
+
+    .header__contact-facebook > a > p:hover {
+        color: var(--foreground);
+    }
+
+    .header__contact-phone > img, .header__contact-facebook > img {
         width: 30px;
     }
 
@@ -107,11 +125,11 @@
             font-size: 3.75em;
         }
 
-        .header__phone > p  {
+        .header__contact-phone > p, .header__contact-facebook > a > p  {
             font-size: 1.75em;
         }
 
-        .header__phone > img {
+        .header__contact-phone > img, .header__contact-facebook > img {
             width: 40px;
         }
     }
