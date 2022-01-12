c510418fc42d24ffa857227242ef622cf54c4692
judith lourde <judith.lourde@gmail.com>
2022-01-12 22:18:53 +0100

    typo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,7 +24,7 @@
             <li>
                 <div class="footer__contact-icon">
                     <img src="/svg/email.svg" alt="email-icon">
-                    <p>shahinzatgar@hotmail.com</p>
+                    <p>shahinzargar@hotmail.com</p>
                 </div>  
             </li>
 
